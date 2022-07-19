b694b81504de24384eb592d2af608ae3ee61db45
Felix Ouma <fatonoh@gmail.com>
2022-07-19 03:37:52 +0300

    Add dummy data and install prop types

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+const ships = [
+  {
+    id: 1,
+    rocket_name: 'Falcon1',
+    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, consequatur ut. Distinctio nostrum voluptatibus recusandae eum, velit dolores mollitia nam aliquid consectetur enim corporis perspiciatis repudiandae animi, sed hic tenetur!',
+    flickr_images: 'https://i.imgur.com/DaCfMsj.jpg',
+  },
+  {
+    id: 2,
+    rocket_name: 'Falcon9',
+    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, consequatur ut. Distinctio nostrum voluptatibus recusandae eum, velit dolores mollitia nam aliquid consectetur enim corporis perspiciatis repudiandae animi, sed hic tenetur!',
+    flickr_images: 'https://farm1.staticflickr.com/929/28787338307_3453a11a77_b.jpg',
+  },
+  {
+    id: 3,
+    rocket_name: 'Starship',
+    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, consequatur ut. Distinctio nostrum voluptatibus recusandae eum, velit dolores mollitia nam aliquid consectetur enim corporis perspiciatis repudiandae animi, sed hic tenetur!',
+    flickr_images: 'https://live.staticflickr.com/65535/48954138962_ee541e6755_b.jpg',
+  },
+];
+
+export default ships;
