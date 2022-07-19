263d5ea149ac81fcb21add3c47906189fb00c489
Felix Ouma <fatonoh@gmail.com>
2022-07-19 23:29:40 +0300

    Add fetch rockets from api.spacexdata.com/rockets endpoint

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './slices/missionsSlice';
+import rocketsReducer from './slices/rocketsSlice';
 
 const reducer = {
   missions: missionsReducer,
+  rockets: rocketsReducer,
 };
 
 const store = configureStore({ reducer });
