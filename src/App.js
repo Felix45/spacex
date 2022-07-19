6fa19eb0800d66c25f603a2bb0cc73a7bcb93922
Felix Ouma <fatonoh@gmail.com>
2022-07-19 13:07:04 +0300

    Add MissionsView and mock data to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import RocketsView from './components/RocketsView';
+import MissionsView from './components/MissionsView';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => (
   <div className="App">
     <RocketsView />
+    <MissionsView />
   </div>
 );
 
