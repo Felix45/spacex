0035f4c15a2516d4fb418be11ed27b7d84719f79
BHDHakim <hakim.bhd@gmail.com>
2022-07-19 16:54:02 +0300

    Add fetch missions feature

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import { fetchMissionsThunk } from '../redux/slices/missionsSlice';
 
-const Layout = () => (
-  <main className="pt-5">
-    <Header />
-    <Container className="mt-5">
-      <Outlet />
-    </Container>
-  </main>
-);
+const Layout = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissionsThunk());
+  }, [dispatch]);
+
+  return (
+    <main className="pt-5">
+      <Header />
+      <Container className="mt-5">
+        <Outlet />
+      </Container>
+    </main>
+  );
+};
 
 export default Layout;
