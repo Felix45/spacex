4c5c06000a54cd0124dc03fee1907305eeb2ab26
Felix Ouma <fatonoh@gmail.com>
2022-07-20 11:32:46 +0300

    Refactor fetchRocketsThunk API call

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import { fetchRocketsThunk } from '../redux/slices/rocketsSlice';
 
-const Layout = () => (
-  <main className="pt-5">
-    <Header />
-    <Container className="mt-5">
-      <Outlet />
-    </Container>
-  </main>
-);
+const Layout = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRocketsThunk());
+  }, []);
+
+  return (
+    <main className="pt-5">
+      <Header />
+      <Container className="mt-5">
+        <Outlet />
+      </Container>
+    </main>
+  );
+};
 
 export default Layout;
--- src/components/RocketsView.js
+++ src/components/RocketsView.js
@@ -1,16 +1,9 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import RocketList from './RocketList';
-import { fetchRocketsThunk } from '../redux/slices/rocketsSlice';
 
 const RocketsView = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchRocketsThunk());
-  }, []);
-
   const { rockets } = useSelector((state) => state.rockets);
 
   return (
