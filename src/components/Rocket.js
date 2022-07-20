468bdd47bf0e2e1401731ce0db9a273c38e53653
BHDHakim <hakim.bhd@gmail.com>
2022-07-20 13:11:59 +0300

    Add rocket booking feature

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Badge from 'react-bootstrap/Badge';
+import { book } from '../redux/slices/rocketsSlice';
 
 const Rocket = (props) => {
+  const dispatch = useDispatch();
   const { rocket } = props;
   const {
-    id, rocket_name, description, flickr_images,
+    id, rocket_name, description, flickr_images, booked,
   } = rocket;
+
+  const handleBooking = () => dispatch(book({ id, booked: !booked }));
 
   return (
     <Row className="my-5">
@@ -22,8 +28,15 @@ const Rocket = (props) => {
         <Card>
           <Card.Body>
             <Card.Title>{rocket_name}</Card.Title>
-            <Card.Text>{description}</Card.Text>
-            <Button id={id} variant="primary">Reserve Rocket</Button>
+            <Card.Text>
+              {booked && <Badge className="me-2" bg="info">Reserved</Badge>}
+              {description}
+            </Card.Text>
+            {
+              booked
+                ? <Button variant="outline-secondary" onClick={handleBooking}>Cancel Reservation</Button>
+                : <Button variant="primary" onClick={handleBooking}>Reserve Rocket</Button>
+            }
           </Card.Body>
         </Card>
       </Col>
--- src/redux/slices/rocketsSlice.js
+++ src/redux/slices/rocketsSlice.js
@@ -18,6 +18,13 @@ export const fetchRocketsThunk = createAsyncThunk(
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
+  reducers: {
+    book(state, action) {
+      const index = state.rockets.findIndex((rocket) => rocket.id === action.payload.id);
+
+      state.rockets.splice(index, 1, { ...state.rockets[index], booked: action.payload.booked });
+    },
+  },
   extraReducers: {
     [fetchRocketsThunk.fulfilled]: (state, action) => {
       state.isLoading = false;
@@ -27,5 +34,7 @@ const rocketsSlice = createSlice({
     [fetchRocketsThunk.rejected]: (state) => { state.isFaild = true; },
   },
 });
+
+export const { book } = rocketsSlice.actions;
 
 export default rocketsSlice.reducer;
