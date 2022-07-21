1f1a762c3fb4cd47a7ca42ad1b6dae0429048f94
BHDHakim <hakim.bhd@gmail.com>
2022-07-21 21:29:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './redux';
+import setupStore from './redux';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store()}>
+  <Provider store={setupStore()}>
     <App />
   </Provider>,
 );
